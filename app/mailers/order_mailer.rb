bc41d85fd34f1491cd5418c933881717b942082e
Mergen Battur <mbattur@yahoo.com>
2022-03-23 07:40:59 -0500

    edit mailer text, create order confirmation partial

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,10 +1,12 @@
 class OrderMailer < ApplicationMailer
+  helper DateFormatHelper
+
   default from: ENV["DEFAULT_FROM_EMAIL"]
 
   def ticket_purchase_email(order)
     @user = order.customer
     @order = order
 
-    mail(to: @user.email, subject: "Movie ticket purchase confirmation")
+    mail(to: @user.email, subject: "Movie Tickets Purchase Confirmation")
   end
 end
